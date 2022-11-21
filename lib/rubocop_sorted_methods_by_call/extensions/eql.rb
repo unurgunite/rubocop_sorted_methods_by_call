929896b183eb246f44fbadbee09ee4c8c7816a22
unurgunite <senpaiguru1488@gmail.com>
2022-11-21 17:45:02 +0300

    Refactored docs

--- lib/rubocop_sorted_methods_by_call/extensions/eql.rb
+++ lib/rubocop_sorted_methods_by_call/extensions/eql.rb
@@ -3,17 +3,17 @@
 class Hash # :nodoc:
   # +Hash#eql?+                                       -> TrueClass, FalseClass
   #
-  # This method checks if hashes are identical. Complexly, it turns +self+ 
-  # values as well as +other+ param values into array via override 
-  # Hash#to_a method. At next step it starts iterating through +self+ values to 
+  # This method checks if hashes are identical. Complexly, it turns +self+
+  # values as well as +other+ param values into array via override
+  # Hash#to_a method. At next step it starts iterating through +self+ values to
   # compare general slice of them and values of +other+ hash.
-  # 
+  #
   # @example
   #    def abc
   #      foo
   #      bar
   #    end
-  # 
+  #
   #    # positive case  # negative case
   #      def foo; end     def foo; end
   #      def bar; end     def c; end
@@ -22,7 +22,7 @@ class Hash # :nodoc:
   # @param [Hash] other Hash object.
   # @return [NilClass] if +other+ is not a Hash.
   # @return [TrueClass] if hashes are identical.
-  # @return [FalseClass] if hashes are not identical.
+  # @return [FalseClass] if hashes are not identical or +general_slice+ is not a subset of +arr+.
   def eql?(other)
     return unless other.is_a? Hash
 
--- lib/rubocop_sorted_methods_by_call/extensions/subset.rb
+++ lib/rubocop_sorted_methods_by_call/extensions/subset.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 class Array # :nodoc:
   # +Array#subset?+                                   -> TrueClass, FalseClass
-  # 
+  #
   # This method checks if one array is a subset of another. It pays attention
   # for elements order.
-  # 
+  #
   # @example
   #   a = [1, 2, 3, 4, 5, 6]
   #   subset_1 = [1, 2, 3]
   #   a.subset?(subset_1) #=> true
   #   subset_2 = [1, 3, 2]
   #   a.subset?(subset_2) #=> false
-  # 
+  #
   # @param [Array] arr Some array.
   # @return [TrueClass] if +arr+ is a subset of +self+.
   # @return [FalseClass] if +arr+ is not a subset of +self+.
